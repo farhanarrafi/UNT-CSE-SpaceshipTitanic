c80168e11e4b4d052e6e3fcbdff2c56381a2c6c9
sivani-tutika <sivanitutika4@gmail.com>
2023-09-14 21:40:28 -0500

    basic outline of main page completed

--- spaceship-titanic/src/app/pages/main/main.component.ts
+++ spaceship-titanic/src/app/pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+
+  passengerData: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+    this.passengerData = this.fb.group({
+      PassengerId: '',
+      HomePlanet: [],
+      CryoSleep: false,
+      Cabin: '',
+      Destination: [],
+      Age: 0,
+      VIP: false,
+      Name: '',
+      Transported: false,
+      RoomService: 0,
+      FoodCourt: 0,
+      ShoppingMall: 0,
+      Spa: 0,
+      VRDeck: 0
+    })
+
+    this.passengerData.valueChanges.subscribe();
+  }
+
+  ngOnInit() {
+
+  }
+
+  submit(passengerData: FormGroup){
+    console.log(this.passengerData.value);
+    console.log(JSON.stringify(this.passengerData.value));
+  }
 
 }
