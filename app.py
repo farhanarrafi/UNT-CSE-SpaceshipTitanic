2a463f3a1499335688aabf557763b5b058819224
Farhan Ar Rafi <farhanarrafi@gmail.com>
2023-09-21 01:24:15 -0500

    [SpaceshipTitanic][Deployment][Stage] Added Angular build files
    - Added folders for template and static files
    
    Refs: https://untworkspace.atlassian.net/browse/SST-13
    
    Signed-off-by: Farhan Ar Rafi <farhanarrafi@gmail.com>

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder='src')
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
-secret_key = app.config.get("SECRET_KEY")
+app = Flask(__name__)
+#env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+#app.config.from_object(env_config)
+#secret_key = app.config.get("SECRET_KEY")
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+# special file handlers and error handlers
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico')
 
 @app.route("/about")
 def about():
